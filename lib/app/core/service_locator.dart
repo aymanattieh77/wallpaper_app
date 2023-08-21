3f9c31366ec10725a5b76f3bd7d8f1366271505b
ayman attieh <aymanattieh77@gmail.com>
2023-08-21 21:08:13 +0300

    fix issues #3

--- lib/app/core/service_locator.dart
+++ lib/app/core/service_locator.dart
@@ -15,7 +15,7 @@ import 'package:wallpaper_app/domain/usecase/favourites/get_favourites_usecase.d
 import 'package:wallpaper_app/domain/usecase/favourites/remove_favourite_usecase.dart';
 import 'package:wallpaper_app/domain/usecase/get_random_wallpapers_usecase.dart';
 import 'package:wallpaper_app/domain/usecase/search_photos_usecase.dart';
-import 'package:wallpaper_app/presentation/controllers/favourite_prodvider.dart';
+import 'package:wallpaper_app/presentation/controllers/favourite_provider.dart';
 import 'package:wallpaper_app/presentation/controllers/home_provider.dart';
 import 'package:wallpaper_app/presentation/controllers/search_provider.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wallpaper_app/app/app.dart';
 import 'package:wallpaper_app/app/core/service_locator.dart';
-import 'package:wallpaper_app/presentation/controllers/favourite_prodvider.dart';
+import 'package:wallpaper_app/presentation/controllers/favourite_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
